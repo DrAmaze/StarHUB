ca54502d8b2aa44e704a777421109c3c335012f5
Jacob Butler <jacobbutler@Jacob-Butlers-MacBook-Pro.local>
2019-02-05 11:58:06 -0800

    Combine reducers with root reducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import { reposReducer } from './reposReducer';
+import { errorsReducer } from './errorsReducer';
+
+export default combineReducers({
+  reposReducer,
+  errorsReducer
+});
