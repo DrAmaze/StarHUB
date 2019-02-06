fb8ba3c0dc5d6d3140d60a4a72e6d4720ed9e4bc
Jacob Butler <jacobbutler@JacobButlersMBP.hsd1.ca.comcast.net>
2019-02-06 12:55:50 -0800

    Add return statement to #configureStore() to solve bug

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import configureStore from './store/store';
 import Root from './components/root';
-import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 document.addEventListener('DOMContentLoaded', () => {
   let store = configureStore();
   const root = document.getElementById('root');
-  // ReactDOM.render(<Root store={ store } />, root);
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(<Root store={ store } />, root);
 }); 
 
 // If you want your app to work offline and load faster, you can change
