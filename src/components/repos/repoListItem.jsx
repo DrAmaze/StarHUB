902c26e3b4fdc4ca6ba5a9a7b10792ad4e6628db
Jacob Butler <jacobbutler@JacobButlersMBP.hsd1.ca.comcast.net>
2019-02-28 14:58:10 -0800

    update conditional rendering

--- src/components/repos/repoListItem.jsx
+++ src/components/repos/repoListItem.jsx
@@ -3,7 +3,6 @@ import {
   STAR_REPOSITORY,
   UNSTAR_REPOSITORY
 } from '../../actions/queries';
-import Loading from '../utility/loading';
 import { Mutation } from 'react-apollo';
 
 class RepoListItem extends Component {
@@ -17,6 +16,10 @@ class RepoListItem extends Component {
   render() {
     const { id, viewerHasStarred } = this.props.repository;
 
+    const addStarLoading = <div>Sending the star...</div>;
+    const removeStarLoading = <div>Removing the star...</div>;
+    const errorDiv = err => <div className='error'>{ err }</div>;
+
     return (
       <li>
         <h2>{ this.props.repository.name }</h2>
@@ -24,31 +27,42 @@ class RepoListItem extends Component {
         <h5>{ this.props.repository.url }</h5>
         { !viewerHasStarred ? (
           <Mutation mutation={ STAR_REPOSITORY } variables= {{ id }}>
-            {(addStar, { data, loading, error}) => {
-              if (data) {
-                return <Loading />
+            {(addStar, { data, loading, error }) => {
+              if (loading) {
+                return <div>{ addStarLoading }</div>
               }
-              console.log(data);
+              if (error) {
+                return errorDiv(error);
+              }
               return (
-              <div>
-                Stargazers: { this.props.repository.stargazers.totalCount }
-                <button onClick={ addStar }>
-                  STAR
-                </button>
-              </div>
-            )}}
+                <div>
+                  Stargazers: { this.props.repository.stargazers.totalCount }
+                  <button onClick={ addStar }>
+                    STAR
+                  </button>
+                </div>
+              )
+            }}
           </Mutation>
         ) : (
-            <Mutation mutation={ UNSTAR_REPOSITORY} variables={{ id }}>
-              {(removeStar, { data, loading, error }) => (
+          <Mutation mutation={ UNSTAR_REPOSITORY} variables={{ id }}>
+            {(removeStar, { data, loading, error }) => {
+              if (loading) {
+                return <div>{ removeStarLoading }</div>;
+              }
+              if (error) {
+                return errorDiv(error);
+              }
+              return (
                 <div>
-                  Stargazers: {this.props.repository.stargazers.totalCount}
+                  Stargazers: { this.props.repository.stargazers.totalCount }
                   <button onClick={ removeStar }>
                     UNSTAR
                   </button>
                 </div>
-              )}
-            </Mutation>
+              )
+            }}
+          </Mutation>
         )}
       </li>
     )
